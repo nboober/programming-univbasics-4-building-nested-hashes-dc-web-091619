7a97acd7c3fd1f1f3e7c9e4781c50a49bf1f25c5
Nicholas Boober <nickboober92@gmail.com>
2019-09-09 16:24:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,7 @@ def base_hash
   # keyword "return." Nice and neat.
 	
 	
-	{}
+	{:railroads => {}}
 
 	
 end
@@ -19,7 +19,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-  monopoly = {:railroads => {}}
+  # monopoly = {:railroads => {}}
   
   
 end
