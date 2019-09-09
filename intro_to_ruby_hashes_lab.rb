4a474d5e503fddc37a671dfa522a5bc9ea9a709b
Nicholas Boober <nickboober92@gmail.com>
2019-09-09 16:03:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,34 +1,16 @@
-# describe "We can create complex, nested Hashes by implementing four methods:" do
-#   describe "the base_hash method returns a Hash that" do
-#     it "has one key" do
-#       expect(base_hash.keys.count).to(eq(1), "Make sure you have at least one key defined in the monopoly hash")
-#     end
 
-#     it "the one key is called :railroads" do
-#       expect(base_hash.has_key?(:railroads)).to(be_truthy, "Make sure you have a key called :railroads in the `monopoly` Hash and that :railroads points to a Hash")
-#     end
-
-#     it "the one key, :railroads, points to an empty Hash" do
-#       expect(base_hash[:railroads]).to eq({})
-#     end
-#   end
 
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	hash = {:railroads => {}}
+	
+	
+	{}
+	
 end
 
 
-# describe "the monopoly_with_second_tier method builds on the Hash returned by base_hash, but updates the inner Hash pointed at by :railroads such that the Hash" do
-#     it "has a key called :pieces" do
-#       expect(monopoly_with_second_tier[:railroads].has_key?(:pieces)).to(be_truthy, "Make sure the nested hash :railroads has a key called :pieces")
-#     end
 
-#     it "has a key, :pieces, points to the Integer value 4" do
-#       expect(monopoly_with_second_tier[:railroads][:pieces]).to(eq(4), "Make sure the nested hash :railroads has a key called :pieces whose value is 4")
-#     end
-#   end
 
 def monopoly_with_second_tier
   # When you start writing the implementation for this method, copy the Hash
@@ -40,14 +22,7 @@ def monopoly_with_second_tier
   
 end
 
-  # describe "the monopoly_with_third_tier method builds on the Hash returned by monopoly_with_second_tier, but updates the Hash pointed at by :railroads such that the Hash" do
-  #   it "has a key called :rent_in_dollars" do
-  #     expect(monopoly_with_third_tier[:railroads].has_key?(:rent_in_dollars)).to(be_truthy, ":railroads should point to a Hash with key rent_in_dollars")
-  #   end
 
-  #   it ":rent_in_dollars points to a Hash with four keys" do
-  #     expect(monopoly_with_third_tier[:railroads][:rent_in_dollars].keys.count).to(eq(4), ":railroads :rent_in_dollars Hash should record 4 levels of ownership")
-  #   end
 
 def monopoly_with_third_tier
   # When you start writing the implementation for this method, copy the Hash
@@ -56,25 +31,7 @@ def monopoly_with_third_tier
   # complexity.
 end
 
-# describe "the monopoly_with_fourth_tier method builds on the hash returned by monopoly_with_third_tier, but updates the Hash so that each of the :railroads :names keys points to a Hash with a key \"mortgage_value\" that is a String with a $-sign and an Integer after it" do
 
-#     it "reports reading_railroad's mortgage value as $100" do
-#       expect(monopoly_with_fourth_tier[:railroads][:names][:reading_railroad]["mortgage_value"]).to eq("$100")
-#     end
-
-#     it "sets the 1st key of the :pennsylvania_railroad hash to a string, 'mortgage_value', whose value is also a string, '$200'" do
-#       expect(monopoly_with_fourth_tier[:railroads][:names][:pennsylvania_railroad]["mortgage_value"]).to eq("$200")
-#     end
-
-#     it "sets the 1st key of the :b_and_o_railroad hash to a string, 'mortgage_value', whose value is also a string, '$400'" do
-#       expect(monopoly_with_fourth_tier[:railroads][:names][:b_and_o_railroad]["mortgage_value"]).to eq("$400")
-#     end
-
-#     it "sets the 1st key of the :shortline_railroad hash to a string, 'mortgage_value', whose value is also a string, '$800'" do
-#       expect(monopoly_with_fourth_tier[:railroads][:names][:shortline_railroad]["mortgage_value"]).to eq("$800")
-#     end
-#   end
-# end
 
 
 def monopoly_with_fourth_tier
