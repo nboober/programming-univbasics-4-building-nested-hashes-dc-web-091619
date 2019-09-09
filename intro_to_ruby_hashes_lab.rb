c8131bfc638db202ad70e0cf970f578d08aff76d
Nicholas Boober <nickboober92@gmail.com>
2019-09-09 16:12:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,11 +18,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-  hash = {}
-  
-  hash[:railroads]
-  
-  puts hash
+  hash = {:railroads}
   
   
 end
