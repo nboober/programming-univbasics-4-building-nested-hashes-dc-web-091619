c27665d7c6b8b8cb9e568846316526bdf28f006d
Nicholas Boober <nickboober92@gmail.com>
2019-09-09 16:32:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,7 @@ def monopoly_with_third_tier
   {:railroads => {
     :pieces => 4, 
     :rent_in_dollars => {
-      :names => {}, 
+      :one_piece_owned => 25, 
       :key2 => {}, 
       :key3 => {}, 
       :key4 => {}
